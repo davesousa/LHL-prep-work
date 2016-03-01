1fe0fb4c3a23d66e0d5b0c64faca5def9c6f8850
Dave Sousa <davejsousa@gmail.com>
2016-03-01 18:57:53 +0000

    fizz buzz update

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,11 +1,15 @@
-for count in 1..100
-  if (count % 3 == 0) && (count % 5 == 0)
-      puts "fizzbuzz"
-  elsif count % 3 == 0
-    puts "fizz"
-  elsif count % 5 == 0
-    puts "buzz"
-  else
-    puts "#{count}"
+def fizzbuzz(a,b)
+  for count in a..b
+    if (count % 3 == 0) && (count % 5 == 0)
+        puts "fizzbuzz"
+    elsif count % 3 == 0
+      puts "fizz"
+    elsif count % 5 == 0
+      puts "buzz"
+    else
+      puts "#{count}"
+    end
   end
 end
+
+fizzbuzz(5,100)
